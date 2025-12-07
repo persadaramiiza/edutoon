5a6cebf3b295df08c7b15d3cdfb18de3c23699b4
persadaramiiza <persadaramiiza@gmail.com>
2025-12-07 13:26:30 +0700

    Fix deploy access

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'edutoon-backend',
-      cwd: '/www/wwwroot/edutoon',
-      script: 'dist/backend/main.js',
+      cwd: '/home/edutoon/edutoon',
+      script: 'dist/main.js',   // <== pakai hasil build, BUKAN src
       instances: 1,
       autorestart: true,
       watch: false,
@@ -13,18 +13,6 @@ module.exports = {
         PORT: 3000,
       },
     },
-    {
-      name: 'edutoon-frontend',
-      cwd: '/www/wwwroot/edutoon/frontend',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start --port 3333',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '500M',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
   ],
 };
+
